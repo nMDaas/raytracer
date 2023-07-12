1b9cbda2d46fb7e6fbde7ac4806fa1206327a124
nMDaas <63910563+nMDaas@users.noreply.github.com>
2023-07-12 18:10:49 -0400

    removed code to test controller copy constructor

--- src/Raytracer.cpp
+++ src/Raytracer.cpp
@@ -1,13 +1,9 @@
 #include <iostream> 
 #include "Controller.cpp"
 
-void testMethod(Controller& c) {
-
-}
 
 int main(void)
 {
     Controller controller;
-    testMethod(controller);
     //controller.run();
 }
