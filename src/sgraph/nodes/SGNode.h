163dc5ecf3a97635f9f6337bc1ccda2f56d3e403
nMDaas <63910563+nMDaas@users.noreply.github.com>
2023-07-18 13:44:17 -0400

    Created SGNode.h

---
+++ src/sgraph/nodes/SGNode.h
@@ -0,0 +1,13 @@
+#ifndef _SGNODE_H_
+#define _SGNODE_H_
+
+/*
+    This interface represents a scenegraph node.
+*/
+class SGNode {
+    public:
+    SGNode(){}
+    virtual ~SGNode(){}
+};
+
+#endif
