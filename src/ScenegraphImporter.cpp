c5604b2f906fddcf14315291914868a51e1ff98e
nMDaas <63910563+nMDaas@users.noreply.github.com>
2023-07-17 20:11:53 -0400

    helper methods to parse commands added

--- src/ScenegraphImporter.cpp
+++ src/ScenegraphImporter.cpp
@@ -28,44 +28,92 @@ IScenegraph* ScenegraphImporter::parse(std::istream& input) {
     string command;
     while (inputWithOutComments >> command) {
         if (command == "instance") {
-            std::cout << "instance" << std::endl;
+            parseInstance(inputWithOutComments);
         }
         else if (command == "group") {
-            std::cout << "group" << std::endl;
+            parseGroup(inputWithOutComments);
         }
         else if (command == "leaf") {
-            std::cout << "leaf" << std::endl;
+            parseLeaf(inputWithOutComments);
         }
         else if (command == "material") {
-            std::cout << "material" << std::endl;
+            parseMaterial(inputWithOutComments);
         }
         else if (command == "scale") {
-            std::cout << "scale" << std::endl;
+            parseScale(inputWithOutComments);
         }
         else if (command == "rotate") {
-            std::cout << "rotate" << std::endl;
+            parseRotate(inputWithOutComments);
         }
         else if (command == "translate") {
-            std::cout << "translate" << std::endl;
+            parseTranslate(inputWithOutComments);
         }
         else if (command == "copy") {
-            std::cout << "copy" << std::endl;
+            parseCopy(inputWithOutComments);
         }
         else if (command == "import") {
-            std::cout << "import" << std::endl;
+            parseImport(inputWithOutComments);
         }
         else if (command == "assign-material") {
-            std::cout << "assign-material" << std::endl;
+            parseAssignMaterial(inputWithOutComments);
         }
         else if (command == "add-child") {
-            std::cout << "add-child" << std::endl;
+            parseAddChild(inputWithOutComments);
         }
         else if (command == "assign-root") {
-            std::cout << "assign-root" << std::endl;
+            parseSetRoot(inputWithOutComments);
         }
         else {
             std::cout << "other command: " << command << std::endl;
         }
     }
 
+}
+
+void ScenegraphImporter::parseInstance(istream& input) {
+    std::cout << "instance" << std::endl;
+}
+
+void ScenegraphImporter::parseGroup(istream& input) {
+    std::cout << "group" << std::endl;
+}
+
+void ScenegraphImporter::parseLeaf(istream& input) {
+    std::cout << "leaf" << std::endl;
+}
+
+void ScenegraphImporter::parseMaterial(istream& input) {
+    std::cout << "material" << std::endl;
+}
+
+void ScenegraphImporter::parseScale(istream& input) {
+    std::cout << "scale" << std::endl;
+}
+
+void ScenegraphImporter::parseRotate(istream& input) {
+    std::cout << "rotate" << std::endl;
+}
+
+void ScenegraphImporter::parseTranslate(istream& input) {
+    std::cout << "translate" << std::endl;
+}
+
+void ScenegraphImporter::parseCopy(istream& input) {
+    std::cout << "copy" << std::endl;
+}
+
+void ScenegraphImporter::parseImport(istream& input) {
+    std::cout << "import" << std::endl;
+}
+
+void ScenegraphImporter::parseAssignMaterial(istream& input) {
+    std::cout << "assign-material" << std::endl;
+}
+
+void ScenegraphImporter::parseAddChild(istream& input) {
+    std::cout << "add-child" << std::endl;
+}
+
+void ScenegraphImporter::parseSetRoot(istream& input) {
+    std::cout << "assign-root" << std::endl;
 }
--- src/ScenegraphImporter.h
+++ src/ScenegraphImporter.h
@@ -25,6 +25,19 @@ class ScenegraphImporter {
         private: 
         std::string stripComments(std::istream& input); // to remove lines with comments (lines starting with "#...")
 
+        void parseInstance(istream& input);
+        void parseGroup(istream& input);
+        void parseLeaf(istream& input);
+        void parseMaterial(istream& input);
+        void parseScale(istream& input);
+        void parseRotate(istream& input);
+        void parseTranslate(istream& input);
+        void parseCopy(istream& input);
+        void parseImport(istream& input);
+        void parseAssignMaterial(istream& input);
+        void parseAddChild(istream& input);
+        void parseSetRoot(istream& input);
+
 };
 
 #endif
