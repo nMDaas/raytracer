bbab82b97874705eadccf7cca864f195df855122
nMDaas <63910563+nMDaas@users.noreply.github.com>
2023-07-20 11:31:46 -0400

    implemented ScaleTransform constructor

--- src/sgraph/nodes/ScaleTransform.h
+++ src/sgraph/nodes/ScaleTransform.h
@@ -11,10 +11,15 @@ class ScaleTransform: public TransformNode {
         ParentSGNode *copyNode() {
             return new ScaleTransform(sx,sy,sz,name,scenegraph);
         } 
-        
+
     public:
         ScaleTransform(float sx,float sy,float sz,const std::string& name,IScenegraph *graph) 
                 :TransformNode(name,graph) {
+                this->sx = sx;
+                this->sy = sy;
+                this->sz = sz;
+                glm::mat4 transform = glm::scale(glm::mat4(1.0),glm::vec3(sx,sy,sz));
+                setTransform(transform);
         }
 };
 
