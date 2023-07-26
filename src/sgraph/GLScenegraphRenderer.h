74efc441f04d75d49f095058931d093237605dee
nMDaas <63910563+nMDaas@users.noreply.github.com>
2023-07-26 19:17:19 -0400

    Created GLScenegraphRenderer class

---
+++ src/sgraph/GLScenegraphRenderer.h
@@ -0,0 +1,13 @@
+#ifndef _GLSCENEGRAPHRENDERER_H_
+#define _GLSCENEGRAPHRENDERER_H_
+
+#include "SGNodeVisitor.h"
+
+/*
+    GLScenegraphRenderer is a visitor that implements SGNodeVisitor and will be
+    used to traverse the scenegraph and render it */
+class GLScenegraphRenderer: public SGNodeVisitor {
+
+};
+
+#endif
