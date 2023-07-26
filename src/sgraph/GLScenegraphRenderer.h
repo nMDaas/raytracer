c89d2d10d6eae51f6eb814699bde1c605135dcd3
nMDaas <63910563+nMDaas@users.noreply.github.com>
2023-07-26 19:28:27 -0400

    GLScenegraphRenderer constructor, destructor and copy constructor implemented

--- src/sgraph/GLScenegraphRenderer.h
+++ src/sgraph/GLScenegraphRenderer.h
@@ -1,5 +1,6 @@
 #ifndef _GLSCENEGRAPHRENDERER_H_
 #define _GLSCENEGRAPHRENDERER_H_
+#include <iostream>
 
 #include "SGNodeVisitor.h"
 
@@ -7,6 +8,19 @@
     GLScenegraphRenderer is a visitor that implements SGNodeVisitor and will be
     used to traverse the scenegraph and render it */
 class GLScenegraphRenderer: public SGNodeVisitor {
+    public:
+    GLScenegraphRenderer() {
+        std::cout << "scenegraph constructor called" << std::endl;
+    }
+
+    ~GLScenegraphRenderer() {}
+
+    /*
+        to check for copy constructors
+    */
+    GLScenegraphRenderer (GLScenegraphRenderer &t) {
+        std::cout << "GLScenegraphRenderer COPY CONSTRUCTOR CALLED" << std::endl;
+    }
 
 };
 
