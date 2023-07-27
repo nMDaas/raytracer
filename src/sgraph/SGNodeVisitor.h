3179fc7630c05c08ce851cef372c7b7cb825633d
nMDaas <63910563+nMDaas@users.noreply.github.com>
2023-07-26 20:06:31 -0400

    declared visitGroupNode() in GLScenegraphRenderer

--- src/sgraph/SGNodeVisitor.h
+++ src/sgraph/SGNodeVisitor.h
@@ -12,11 +12,14 @@ class TranslateTransform;
 class SGNodeVisitor {
         public:
         virtual void visitGroupNode(GroupNode *node)=0;
+
+        /*
         virtual void visitLeafNode(LeafNode *node)=0;
         virtual void visitTransformNode(TransformNode *node)=0;
         virtual void visitScaleTransform(ScaleTransform *node)=0;
         virtual void visitTranslateTransform(TranslateTransform *node)=0;
         virtual void visitRotateTransform(RotateTransform *node)=0;
+        */
 };
 
 #endif
