05a6536546b21fa9ccb81a6fd651dae3a00403ce
nMDaas <63910563+nMDaas@users.noreply.github.com>
2023-07-30 16:45:47 -0400

    std::couts from GLScenegraphRenderer removed

--- src/sgraph/GLScenegraphRenderer.cpp
+++ src/sgraph/GLScenegraphRenderer.cpp
@@ -6,7 +6,7 @@
 
 GLScenegraphRenderer::GLScenegraphRenderer(stack<glm::mat4>& mv, map<string,util::ObjectInstance *>& os, util::ShaderLocationsVault& shaderLocs) 
     : modelview(mv), objects(os), shaderLocations(shaderLocs) {
-    std::cout << "scenegraph constructor called" << std::endl;
+    //std::cout << "scenegraph constructor called" << std::endl;
 }
 
 GLScenegraphRenderer::~GLScenegraphRenderer() {}
@@ -19,29 +19,29 @@ GLScenegraphRenderer::GLScenegraphRenderer (GLScenegraphRenderer &t) : modelview
 }
 
 void GLScenegraphRenderer::visitGroupNode(GroupNode *groupNode) {
-    std::cout << "GroupNode: " << groupNode->getName() << std::endl;
+    //std::cout << "GroupNode: " << groupNode->getName() << std::endl;
     for (int i=0;i<groupNode->getChildren().size();i=i+1) {
         groupNode->getChildren()[i]->accept(this);
     }
 }
 
 void GLScenegraphRenderer::visitScaleTransform(ScaleTransform *scaleNode) {
-    std::cout << "ScaleTransform: " << scaleNode->getName() << std::endl;
+    //std::cout << "ScaleTransform: " << scaleNode->getName() << std::endl;
     visitTransformNode(scaleNode);
 }
 
 void GLScenegraphRenderer::visitRotateTransform(RotateTransform *rotateNode) {
-    std::cout << "RotateTransform: " << rotateNode->getName() << std::endl;
+    //std::cout << "RotateTransform: " << rotateNode->getName() << std::endl;
     visitTransformNode(rotateNode);
 }
 
 void GLScenegraphRenderer::visitTranslateTransform(TranslateTransform *translateNode) {
-    std::cout << "TranslateTransform: " << translateNode->getName() << std::endl;
+    //std::cout << "TranslateTransform: " << translateNode->getName() << std::endl;
     visitTransformNode(translateNode);
 }
 
 void GLScenegraphRenderer::visitTransformNode(TransformNode *transformNode) {
-    std::cout << "Transform: " << transformNode->getName() << std::endl;
+    //std::cout << "Transform: " << transformNode->getName() << std::endl;
     modelview.push(modelview.top());
     modelview.top() = modelview.top() * transformNode->getTransform();
     if (transformNode->getChildren().size()>0) {
@@ -51,7 +51,7 @@ void GLScenegraphRenderer::visitTransformNode(TransformNode *transformNode) {
 }
 
 void GLScenegraphRenderer::visitLeafNode(LeafNode *leafNode) {
-    std::cout << "LeafNode to draw: " << leafNode->getName() << std::endl;
+    //std::cout << "LeafNode to draw: " << leafNode->getName() << std::endl;
      //send modelview matrix to GPU  
     glUniformMatrix4fv(shaderLocations.getLocation("modelview"), 1, GL_FALSE, glm::value_ptr(modelview.top()));
     glUniform4fv(shaderLocations.getLocation("vColor"),1,glm::value_ptr(leafNode->getMaterial().getAmbient()));
