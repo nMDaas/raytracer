c776cb28426e8b11af87704d099d7acf9b600f23
nMDaas <63910563+nMDaas@users.noreply.github.com>
2023-07-30 16:36:50 -0400

    protected variable "light" added to LightNode

--- src/sgraph/nodes/LightNode.h
+++ src/sgraph/nodes/LightNode.h
@@ -1,12 +1,15 @@
 #ifndef _LIGHTNODE_H_
 #define __LIGHTNODE_H_
 
+#include "Light.h"
+
 #include "ChildSGNode.h"
 #include "../IScenegraph.h"
 #include "SGNode.h"
 
 class LightNode: public ChildSGNode {
     protected: 
+    util::Light light;
 
     public: 
     LightNode(const std::string& name,IScenegraph *graph);
