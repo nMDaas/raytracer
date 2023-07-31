88db9d3b0e6593636760f63aea2b359e22cd9b0c
nMDaas <63910563+nMDaas@users.noreply.github.com>
2023-07-31 09:19:21 -0400

    light set up in LightNode constructor

--- src/sgraph/nodes/LightNode.cpp
+++ src/sgraph/nodes/LightNode.cpp
@@ -2,9 +2,11 @@
 
 LightNode::LightNode(const std::string& name,IScenegraph *graph,glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular)
     :ChildSGNode(name,graph) {
+    light = new util::Light();
     light->setAmbient(ambient.x,ambient.y,ambient.z);
     light->setDiffuse(diffuse.x,diffuse.y,diffuse.z);
     light->setSpecular(specular.x,specular.y,specular.z);
+    
 }
 
 SGNode* LightNode::clone() {
