91ee6eda3ec05217cc390011d0c7d86030c7c1a7
nMDaas <63910563+nMDaas@users.noreply.github.com>
2023-08-02 15:42:58 -0400

    Raytracer calls meshMaterial setup in different ways depending on whether run on debugging mode or terminal

--- src/Raytracer.cpp
+++ src/Raytracer.cpp
@@ -9,6 +9,14 @@ int main(int argc,char *argv[])
     Model model;
     View view;
     Controller controller(model, view);
+
     controller.meshMaterialSetup(argv[1]);
+
+    // when running via debug: 
+    //controller.meshMaterialSetup("scenegraphs/simpleScene.txt");
+
+    // when running via terminal: 
+    // controller.meshMaterialSetup("src/scenegraphs/simpleScene.txt");
+
     controller.run();
 }
