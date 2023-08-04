59ce54d52bc66431481caf7749ead1d9ebab703b
nMDaas <63910563+nMDaas@users.noreply.github.com>
2023-08-04 10:39:15 -0400

    created class LightLocation

---
+++ include/LightLocation.h
@@ -0,0 +1,13 @@
+#ifndef ___LIGHTLOCATION_H___
+#define ___LIGHTLOCATION_H___
+
+class LightLocation {
+    public: 
+    int ambient;
+
+    LightLocation() {
+        ambient = -1;
+    }
+};
+
+#endif
