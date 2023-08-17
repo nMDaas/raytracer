be12796b5f7e38946458a1cf03ab3dc29776026d
nMDaas <63910563+nMDaas@users.noreply.github.com>
2023-08-17 10:44:57 -0400

    implemented Box calcTime()

--- src/objects/Box.cpp
+++ src/objects/Box.cpp
@@ -9,3 +9,44 @@ Box::~Box(){}
 Box::Box (Box &t){
     std::cout << "BOX COPY CONSTRUCTOR CALLED" << std::endl;
 }
+
+bool Box::calcTime(glm::vec4 s, glm::vec4 v) {
+    float tMinX = (vmin.x - s.x)/v.x; 
+    float tMaxX = (vmax.x - s.x)/v.x;
+
+    // swaps account for different ray directions
+    if (tMinX > tMaxX) {
+        std::swap(tMinX, tMaxX); 
+    }
+
+    float tMinY = (vmin.y - s.y)/v.y; 
+    float tMaxY = (vmax.y - s.y)/v.y; 
+
+    if (tMinY > tMaxY) {
+        std::swap(tMinY, tMaxY); 
+    }
+
+    tmin = (tMinX > tMinY) ? tMinX : tMinY;
+    tmax = (tMaxX < tMaxY) ? tMaxX : tMaxY;
+
+    // ray missed box
+    if ((tMinX > tMaxY) || (tMinY > tMaxX)) {
+        return false;
+    } 
+
+    float tMinZ = (vmin.z - s.z)/v.z; 
+    float tMaxZ = (vmax.z - s.z)/v.z; 
+
+    if (tMinZ > tMaxZ) {
+        std::swap(tMinZ, tMaxZ); 
+    }
+
+    // ray missed box
+    if ((tmin > tMaxZ) || (tMinZ > tmax)) {
+        return false;
+    }
+
+    tmin = (tMinZ > tmin) ? tMinZ : tmin;
+    tmax = (tMaxZ < tmax) ? tMaxZ : tmax;
+}
+}
--- src/objects/Box.h
+++ src/objects/Box.h
@@ -9,7 +9,13 @@ class Box {
         ~Box();
 
         Box (Box &t); // to check for copy constructors
+        bool calcTime(glm::vec4 s, glm::vec4 v); 
 
+    private:
+        glm::vec3 vmin = glm::vec3(-0.5f,-0.5f,-0.5f); // minimum bounds of box in Object Coordinate System
+        glm::vec3 vmax = glm::vec3(0.5f,0.5f,0.5f); // maximum bounds of box in Object Coordinate System
+        float tmin;
+        float tmax;
 };
 
 #endif
