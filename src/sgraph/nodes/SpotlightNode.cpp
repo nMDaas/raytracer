b636d7abde362a161c038ed1b47681d8041cdcfb
nMDaas <63910563+nMDaas@users.noreply.github.com>
2023-09-10 20:25:27 -0400

    deleted isSpotlight() method in SpotlightNode

--- src/sgraph/nodes/SpotlightNode.cpp
+++ src/sgraph/nodes/SpotlightNode.cpp
@@ -3,9 +3,5 @@
 SpotlightNode::SpotlightNode(const std::string& name,IScenegraph *graph,glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular,glm::vec4 direction,float spotAngle) 
 :LightNode(name,graph,ambient,diffuse,specular) {
     light->setSpotDirection(direction.x,direction.y,direction.z);
-    light->setSpotAngle(spotAngle);
-}
-
-bool SpotlightNode::isSpotlight() {
-    return true;
+    light->setSpotAngle(glm::radians(spotAngle));
 }
--- src/sgraph/nodes/SpotlightNode.h
+++ src/sgraph/nodes/SpotlightNode.h
@@ -6,7 +6,6 @@
 class SpotlightNode: public LightNode {
     public: 
     SpotlightNode(const std::string& name,IScenegraph *graph,glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular,glm::vec4 direction,float spotAngle);
-    bool isSpotlight(); 
 };
 
 #endif
