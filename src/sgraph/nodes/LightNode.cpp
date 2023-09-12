013eb45283475ffa8170b02a01d05d848cc747bc
nMDaas <63910563+nMDaas@users.noreply.github.com>
2023-09-11 22:07:29 -0400

    implemented generateLightCells() in LightNode

--- src/sgraph/nodes/LightNode.cpp
+++ src/sgraph/nodes/LightNode.cpp
@@ -8,6 +8,7 @@ LightNode::LightNode(const std::string& name,IScenegraph *graph,glm::vec3 ambien
     light->setDiffuse(diffuse.x,diffuse.y,diffuse.z);
     light->setSpecular(specular.x,specular.y,specular.z);
     
+    generateLightCells(ambient,diffuse,specular);
 }
 
 SGNode* LightNode::clone() {
@@ -21,4 +22,27 @@ void LightNode::accept(SGNodeVisitor* visitor) {
 
 util::Light* LightNode::getLight() {
     return light;
+}
+
+void LightNode::generateLightCells(glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular) {
+    lightCells.push_back(*light); //main light at bottom left corner (x,y,z)
+    util::Light light2; //light above (x,y+1,z)
+    light2.setPosition(0.0f,1.0f,0.0f);
+    light2.setAmbient(ambient.x,ambient.y,ambient.z);
+    light2.setDiffuse(diffuse.x,diffuse.y,diffuse.z);
+    light2.setSpecular(specular.x,specular.y,specular.z);
+    util::Light light3; //light across (x+1,y+1,z)
+    light3.setPosition(1.0f,1.0f,0.0f);
+    light3.setAmbient(ambient.x,ambient.y,ambient.z);
+    light3.setDiffuse(diffuse.x,diffuse.y,diffuse.z);
+    light3.setSpecular(specular.x,specular.y,specular.z);
+    util::Light light4; //light on right (x+1,y,z)
+    light4.setPosition(1.0f,0.0f,0.0f);
+    light4.setAmbient(ambient.x,ambient.y,ambient.z);
+    light4.setDiffuse(diffuse.x,diffuse.y,diffuse.z);
+    light4.setSpecular(specular.x,specular.y,specular.z);
+    lightCells.push_back(light2);
+    lightCells.push_back(light3);
+    lightCells.push_back(light4);
+}
 }
--- src/sgraph/nodes/LightNode.h
+++ src/sgraph/nodes/LightNode.h
@@ -10,15 +10,16 @@
 class LightNode: public ChildSGNode {
     protected: 
     util::Light* light;
+    std::vector<util::Light> lightCells;
 
     public: 
     LightNode(const std::string& name,IScenegraph *graph,glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular);
 
     SGNode *clone();
-
     void accept(SGNodeVisitor* visitor);
-
     util::Light* getLight();
+    private:
+    void generateLightCells(glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular); // currently generating 2x2 cells
 };
 
 #endif
