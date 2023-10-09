de79deefe30bd951f9989cc5c14ccd4167e181e3
nMDaas <63910563+nMDaas@users.noreply.github.com>
2023-10-08 21:57:05 -0400

    implemented ObjImporter parseObjFile() and stripComments()

--- src/ObjImporter.cpp
+++ src/ObjImporter.cpp
@@ -8,3 +8,26 @@ ObjImporter::~ObjImporter() {}
 ObjImporter::ObjImporter (ObjImporter &t) {
     std::cout << "OBJ IMPORTER COPY CONSTRUCTOR CALLED" << std::endl;
 }
+
+void ObjImporter::parseObjFile(std::istream& input) {
+    string inputWithOutCommentsString = stripComments(input); // remove comments
+    istringstream inputWithOutComments(inputWithOutCommentsString);
+    string command;
+    while (inputWithOutComments >> command) {
+        std::cout << command << " "; 
+    }
+}
+
+std::string ObjImporter::stripComments(std::istream& input) {
+    std::string line;
+    stringstream clean;
+    while (getline(input,line)) {
+        int i=0;
+        while ((i<line.length()) && (line[i]!='#')) {
+            clean << line[i];
+            i++;
+        }
+        clean << endl;
+    }
+    return clean.str();
+}
--- src/ObjImporter.h
+++ src/ObjImporter.h
@@ -2,6 +2,11 @@
 #define _OBJIMPORTER_H_
 
 #include <iostream>
+#include <istream>
+#include <fstream>
+#include <string>
+#include <sstream>
+using namespace std;
 
 class ObjImporter {
     public:
@@ -11,7 +16,10 @@ class ObjImporter {
         // to check for copy constructors
         ObjImporter (ObjImporter &t);
 
+        void parseObjFile(std::istream& input);
+
     private: 
+        std::string stripComments(std::istream& input);
 };
 
 #endif
